f20840b867e7f89a2c0d11f4aa20738ae1041190
super-sydney <skho2003@gmail.com>
2022-01-11 13:38:05 +0100

    """"matchmaking""""

--- app.js
+++ app.js
@@ -21,16 +21,50 @@ const players = new Map(); //map ws to id
 wss.on("connection", (ws) => {
     var id = 0;
     ws.on("open", (e) => {
+        players.put(ws, id++) //assign an id to this ws
 
+        let foundGame = false;
+        for (game of games.entries) { //find a non-empty game to add this player to
+            if (!game.isFull()) {
+                foundGame = true;
+                game.addPlayer(ws);
+                break;
+            }
+        }
+
+        if (!foundGame) { //if no game was found, make a new game and add it
+            let g = new Game();
+            g.addPlayer(ws);
+            games.put(players.get(ws), g);
+        }
     });
 
     ws.on("message", (msg) => {
 
     });
 
-    ws.on("close", (e) => {
-
+    ws.on("close", (e) => { //remove player from list of players and ongoing games
+        let playerId = players.get(ws);
+        games.delete(playerId);
+        players.delete(ws);
     })
 });
+
+class Game {
+    players = [];
+    constructor() {
+
+    }
+
+    addPlayer(ws) {
+        players.push(ws);
+    }
+
+    isFull() {
+        return players.length >= 2;
+    }
+
+    //add actual code for the game into here
+}
 
 server.listen(port);
